8eb3d0ebfc6d649da5ba1aaa5d6a57e702241744
John Hunter <john@pushagency.io>
2017-05-10 10:27:57 -0700

    added tests

--- src/test/scala/stackoverflow/StackOverflowSuite.scala
+++ src/test/scala/stackoverflow/StackOverflowSuite.scala
@@ -9,6 +9,8 @@ import org.apache.spark.SparkContext._
 import org.apache.spark.rdd.RDD
 import java.io.File
 
+import stackoverflow.StackOverflow._
+
 
 
 trait SharedSparkContext extends BeforeAndAfterAll { self:Suite =>
@@ -33,8 +35,13 @@ trait SharedSparkContext extends BeforeAndAfterAll { self:Suite =>
 
 
 @RunWith(classOf[JUnitRunner])
-class StackOverflowSuite extends FunSuite with BeforeAndAfterAll {
+class StackOverflowSuite extends FunSuite with SharedSparkContext {
 
+  def filePath = {
+    val resource = this.getClass.getClassLoader.getResource("stackoverflow/stackoverflow.csv")
+    if (resource == null) sys.error("Please download the dataset as explained in the assignment instructions")
+    new File(resource.toURI).getPath
+  }
 
   lazy val testObject = new StackOverflow {
     override val langs =
@@ -55,6 +62,24 @@ class StackOverflowSuite extends FunSuite with BeforeAndAfterAll {
       case _: Throwable => false
     }
     assert(instantiatable, "Can't instantiate a StackOverflow object")
+  }
+
+
+  test("test data rdds") {
+    val dataRdd = sc.textFile(filePath)
+    assert(dataRdd.count() == 8143801)
+
+    val raw = rawPostings(dataRdd)
+    assert(raw.count() == 8143801)
+
+    val grouped = groupedPostings(raw)
+    assert(grouped.count() == 2121822)
+
+    val scored  = scoredPostings(grouped)
+    assert(scored.count() == 2121822)
+
+    val vectors = vectorPostings(scored)
+    assert(vectors.count() == 2121822)
   }
 
 
